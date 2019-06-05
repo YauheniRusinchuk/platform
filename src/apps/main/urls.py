d7cd63fb143f0649d41cf2b7eeac6cf42570e825
YauheniRusinchuk <ruevgal@gmail.com>
2019-06-05 13:07:48 +0300

    add home page

--- src/apps/main/urls.py
+++ src/apps/main/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import index
 from django.views.generic import TemplateView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('home/', include('src.apps.home.urls', namespace='home')),
     path('', index, name='main_page'),
     path('about/', TemplateView.as_view(template_name='about/about.html'), name='about')
 ]
